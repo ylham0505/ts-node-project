d9e2f59b5d08ae41b7c5ac78620f71a2e23c6d39
ylham0505 <serdar.garyagdyyew204@gmail.com>
2026-04-30 17:42:12 +0500

    docker

--- src/models/category.ts
+++ src/models/category.ts
@@ -3,6 +3,7 @@ export interface ICategory extends Document {
     name: string;
     name_ru?: string;
     name_en?: string;
+    products: mongoose.Types.ObjectId[];
     
 }
  
@@ -10,6 +11,7 @@ const CategorySchema: Schema<ICategory> = new Schema({
     name: { type: String, required: true },
     name_ru: { type: String, unique: true },
     name_en: { type: String, unique: true },
+    products: [{ type: Schema.Types.ObjectId, ref: 'Product' }],
 
 }, { timestamps: true },
 );
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config(); // Загружаем .env файл
 import mongoose from 'mongoose';
 import app from './app';
 import { seedAdmin } from './config/seedAdmin';
@@ -6,7 +7,7 @@ import { seedAdmin } from './config/seedAdmin';
 dotenv.config(); // Загружаем .env файл
 
 const PORT = process.env["PORT"] || 5000;
-const MONGO_URI = process.env['MONGO_URI'] || 'mongodb://0.0.0.0:27017/typescript';
+const MONGO_URI = process.env['MONGO_URI'] || 'mongodb://localhost:27017/typescript';
 
 if (!MONGO_URI) {
   throw new Error('MONGO_URI is not defined in .env file');
